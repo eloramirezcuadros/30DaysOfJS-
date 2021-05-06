460eac8a90053a7c9f087999fd453eb0403f5335
Luisa Ramirez C <vaddos17@gmail.com>
2021-05-06 11:15:09 -0500

    fist day challenge of JavaScript

---
+++ day1OfJS/datatypes.js
@@ -0,0 +1,10 @@
+/*
+Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+console.log(typeof "Luisa");
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+*/
---
+++ day1OfJS/variable.js
@@ -0,0 +1,65 @@
+/*
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let vr = "javascript";
+
+let tr = true;
+let fl = false; 
+
+let firstName
+console.log(firstName);
+
+let emptyValue = null;
+*/
+
+
+/*
+Declare four variables without assigning values
+
+let first
+let second
+let third
+let fouth
+*/
+
+
+/*
+Declare four variables with assigned values
+
+let first = 1;
+let second = 2;
+let third = 3;
+let fouth = 4;
+*/
+
+
+/*
+Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let firstName
+let lastName 
+let maritalStatus 
+let country  
+let age
+*/
+
+
+/*
+Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName; let lastName; let maritalStatus; let country; let age;
+*/
+
+
+/*
+Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+I am 25 years old.
+You are 30 years old.
+
+let myAge = 25;
+console.log(`I am ${myAge} years old`);
+
+let yourAge = 30;
+console.log(`You are ${yourAge} years old`);
+*/
