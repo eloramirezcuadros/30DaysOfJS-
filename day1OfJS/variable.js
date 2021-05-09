ea4858644a2ba266b48fe9f4c1d4f30dc60125a6
Luisa Ramirez C <vaddos17@gmail.com>
2021-05-09 12:05:55 -0500

    Changed variables

--- day1OfJS/variable.js
+++ day1OfJS/variable.js
@@ -1,10 +1,10 @@
 /*
 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
-const vr = "javascript";
-const tr = true;
-const fl = false; 
-const firstName
+const variabl_e = "javascript";
+const tru_e = true;
+const fals_e = false; 
+const firstName;
 console.log(firstName);
 const emptyValue = null;
 */
